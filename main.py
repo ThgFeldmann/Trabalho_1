ecfae3995701a8d7b5af17bf818dbc482e1f9dcd
Thiago Feldmann da Rosa <thiagofeldmann06@gmail.com>
2026-03-16 15:29:03 -0300

    add: items de exemplo
    Também foram removidos algumas linhas que não seriam usadas junto com pequenas mudanças em alguns comentários

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+#* Classe para Livros
 class Livro:
     def __init__(
                 self, 
@@ -20,28 +21,50 @@ class Livro:
         self.estoque = estoque
     
     def Info(self): #* Método para informar *todos* os livros
+
         # Calculo para o valor total em estoque
         valor_estoque = self.valor * self.estoque
         
-        print("-"*30)
+        print(""*30)
         print(f">>>>>>Cod#{self.codigo}")
         print(f"Titulo/Editora: {self.titulo}/{self.editora}")
         print(f"Categoria: {self.categoria}")
         print(f"Ano: {self.ano}")
-        print(f"Valor: R$ {self.valor:.2f}")
+        print(f"Valor: R$ {self.valor}")
         print(f"Estoque: {self.estoque} unidade(s)")
-        print(f"Valor total em estoque: R$ {valor_estoque:.2f}")
+        print(f"Valor total em estoque: R$ {valor_estoque}")
 
 #* main function
 if __name__ == "__main__":
     lista_livros = []
+    lista_livros.append(
+                        Livro(
+                            codigo="0301", 
+                            titulo="Compiladores",
+                            editora="Pearson",
+                            categoria="Computação",
+                            ano="2016",
+                            valor=85.00,
+                            estoque=125
+                        ))
+    lista_livros.append(
+                        Livro(
+                            codigo="1203", 
+                            titulo="Engenharia de Software",
+                            editora="Pressman",
+                            categoria="Computação",
+                            ano="2011",
+                            valor=78.00,
+                            estoque=100
+                        ))
+
     running = True
     escolha = 0
     
     print("-"*30)
     print("\tSistema Livraria")
     
-    # Condition to run the system
+    #* Condition to run the system
     while running:
         print("-"*30)
         print("Funcionalidades Disponíveis: \n")
@@ -54,50 +77,46 @@ if __name__ == "__main__":
         print("7 - Valor total no estoque")
         print("0 - Encerrar atividades\n")
         
-        try:
-            escolha = int(input(
-                "Digite o número da funcionalidade desejada: "))
+        escolha = int(input(
+            "Digite o número da funcionalidade desejada: "))
 
-            if escolha == 0: # Encerrar o sistema
-                print("-"*30)
-                print("Encerrando atividades...")
-                print("-"*30)
-                running = False
-
-            elif escolha == 1: # Cadastro de livros
-                print("-"*30)
-                print("Executar função de cadastro")
-
-            elif escolha == 2: # Listagem geral dos livros
-                print("-"*30)
-                print("Executar função de listagem")
-                Livro.Info()
-
-            elif escolha == 3: # Buscar livro por nome
-                print("-"*30)
-                print("Executar função de busca de livro por nome")
-
-            elif escolha == 4: # Buscar livro por categoria
-                print("-"*30)
-                print("Executar função de busca de livro por categoria")
-
-            elif escolha == 5: # Buscar livro por preço
-                print("-"*30)
-                print("Executar função de busca de livro por preço")
-
-            elif escolha == 6: # Buscar livro por estoque
-                print("-"*30)
-                print("Executar função de buscar livros por estoque")
-
-            elif escolha == 7: # Valor total no estoque
-                print("-"*30)
-                print("Executar função de calculo do estoque")
-
-            else: # Opção inválida
-                print("-"*30)
-                print("Opção não encontrada")
-
-        except Exception as error:
+        if escolha == 0: # Encerrar o sistema
             print("-"*30)
-            print("Ocorreu um erro, certifique-se de" +
-                " digitar um número dentre as opções disponíveis")
+            print("Encerrando atividades...")
+            print("-"*30)
+            running = False
+
+        elif escolha == 1: # Cadastro de livros
+            print("-"*30)
+            print("Executar função de cadastro")
+
+        elif escolha == 2: # Listagem geral dos livros
+            print("-"*30)
+            for livro in lista_livros:
+                livro.Info()
+
+        elif escolha == 3: # Buscar livro por nome
+            print("-"*30)
+            print("Executar função de busca de livro por nome")
+
+        elif escolha == 4: # Buscar livro por categoria
+            print("-"*30)
+            print("Executar função de busca de livro por categoria")
+
+        elif escolha == 5: # Buscar livro por preço
+            print("-"*30)
+            print("Executar função de busca de livro por preço")
+
+        elif escolha == 6: # Buscar livro por estoque
+            print("-"*30)
+            print("Executar função de buscar livros por estoque")
+
+        elif escolha == 7: # Valor total no estoque
+            print("-"*30)
+            print("Executar função de calculo do estoque")
+
+        else: # Opção inválida
+            print("-"*30)
+            print("Opção não encontrada")
+            
+        input("Continuar...")
