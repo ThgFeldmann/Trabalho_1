67ba410783746229af4f12bfa7346b596caa77cf
Thiago Feldmann da Rosa <thiagofeldmann06@gmail.com>
2026-03-17 14:27:49 -0300

    add: Função de cadastro de livros

--- main.py
+++ main.py
@@ -24,19 +24,94 @@ class Livro:
 
         # Calculo para o valor total em estoque
         valor_estoque = self.valor * self.estoque
-        
-        print(""*30)
-        print(f">>>>>>Cod#{self.codigo}")
+
+        print(f"\n>>>>>>Cod#{self.codigo}")
         print(f"Titulo/Editora: {self.titulo}/{self.editora}")
         print(f"Categoria: {self.categoria}")
         print(f"Ano: {self.ano}")
         print(f"Valor: R$ {self.valor}")
-        print(f"Estoque: {self.estoque} unidade(s)")
-        print(f"Valor total em estoque: R$ {valor_estoque}")
+        print(f"Estoque: {self.estoque:.2f} unidade(s)")
+        print(f"Valor total em estoque: R$ {valor_estoque:.2f}")
+
+#* Funções
+
+def Cadastro(lista_livros):
+    codigo = ""
+    titulo = ""
+    editora = ""
+    categoria = ""
+    ano = ""
+    valor = 0.00
+    estoque = 0
+    valor_estoque = 0.00
+    confirmação = ""
+    
+    print("Cadastro\n")
+    codigo = input("Digite o código do livro: ")
+    titulo = input("Digite o titulo do livro: ")
+    editora = input("Digite a editora do livro: ")
+    categoria = input("Digite a categoria do livro: ")
+    ano = input("Digite o ano de publicação do livro: ")
+    valor = float(input("Digite o valor da unidade deste livro: "))
+    estoque = int(input("Digite a quantidade em estoque: "))
+    
+    # Calculo para o valor total em estoque
+    valor_estoque = valor * estoque
+    
+    print("-"*30)
+    print("Confirmando os dados...")
+    print(f"\n>>>>>>Cod#{codigo}")
+    print(f"Titulo/Editora: {titulo}/{editora}")
+    print(f"Categoria: {categoria}")
+    print(f"Ano: {ano}")
+    print(f"Valor: R$ {valor}")
+    print(f"Estoque: {estoque:.2f} unidade(s)")
+    print(f"Valor total em estoque: R$ {valor_estoque:.2f}\n")
+    
+    print("-"*30)
+    
+    print("Cadastrar?\n")
+    print("S - Confirmar")
+    print("N - Cancelar\n")
+    confirmação = input("Escolha: ").upper()
+    
+    if confirmação == "S":
+        print("-"*30)
+        print("\nCadastrando o livro...")
+        print(".")
+        print(".")
+        print(".")
+
+        try:
+            lista_livros.append(Livro(
+                codigo=codigo,
+                titulo=titulo,
+                editora=editora,
+                categoria=categoria,
+                ano=ano,
+                valor=valor,
+                estoque=estoque
+            ))
+            
+            print("Livro cadastrado com sucesso.")
+        except Exception as error:
+            print("Ocorreu um no cadastro, tente novamente mais tarde.")
+            print("Erro: ", error)
+    
+    elif confirmação == "N":
+        print("-"*30)
+        print("Cancelando o cadastro...")
+        print(".")
+        print(".")
+        print(".")
+        print("Cadastro cancelado.")
 
 #* main function
 if __name__ == "__main__":
+
     lista_livros = []
+    
+    # Livros de exemplos
     lista_livros.append(
                         Livro(
                             codigo="0301", 
@@ -89,6 +164,7 @@ if __name__ == "__main__":
         elif escolha == 1: # Cadastro de livros
             print("-"*30)
             print("Executar função de cadastro")
+            Cadastro(lista_livros)
 
         elif escolha == 2: # Listagem geral dos livros
             print("-"*30)
@@ -119,4 +195,4 @@ if __name__ == "__main__":
             print("-"*30)
             print("Opção não encontrada")
             
-        input("Continuar...")
+        input("\nContinuar...")
